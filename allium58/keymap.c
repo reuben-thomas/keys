4585c49dedd6c0dc598be7f9d67ba4c594a52ff7
Reuben Thomas <reubenthomas123@gmail.com>
2026-02-24 21:08:46 +0000

    Improve mouse control

--- allium58/keymap.c
+++ allium58/keymap.c
@@ -1,12 +1,17 @@
 #include QMK_KEYBOARD_H
 
 enum layer_number {
-    _QWERTY = 0,
-    _LOWER  = 1,
-    _RAISE  = 2,
-    _ADJUST = 3,
+  _QWERTY = 0,
+  _LOWER = 1,
+  _RAISE = 2,
+  _ADJUST = 3,
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+  mouse_report.h = -mouse_report.h;
+  return mouse_report;
 }
